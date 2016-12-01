9b33cae59d3b169a85e5551069b616e1c29cf6ac
Nathan Smith <logan33575@gmail.com>
2016-12-01 16:08:24 -0600

    commit Building escape #5

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -18,8 +18,9 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-    UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair"));
+    
+    FString ObjectName = GetOwner()->GetName();
+    UE_LOG(LogTemp, Warning, TEXT("Position for %s"), *ObjectName);
   	
 }
 
